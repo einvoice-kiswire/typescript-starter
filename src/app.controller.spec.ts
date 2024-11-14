552b8292d31793882b0f07ab5a6dffeb848e9dcb
edptemp3 <einvoice-noreply@kiswire.com>
2024-11-14 08:50:09 +0800

    disable prettier

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,8 +1,9 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+/* eslint-disable prettier/prettier */
+import { Test, TestingModule } from "@nestjs/testing";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
 
-describe('AppController', () => {
+describe("AppController", () => {
   let app: TestingModule;
 
   beforeAll(async () => {
@@ -12,10 +13,10 @@ describe('AppController', () => {
     }).compile();
   });
 
-  describe('getHello', () => {
+  describe("getHello", () => {
     it('should return "Hello World!"', () => {
       const appController = app.get(AppController);
-      expect(appController.getHello()).toBe('Hello World!');
+      expect(appController.getHello()).toBe("Hello World!,hehe");
     });
   });
 });
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+/* eslint-disable prettier/prettier */
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
 
 @Module({
   imports: [],
